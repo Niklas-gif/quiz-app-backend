5ef24251f52fdf75ce06bf2c30d354c098b0e35c
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-04-01 09:11:53 +0200

    gin test

--- main.go
+++ main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	//var answer = models.Answer{Description: "Test", IsCorrect: true}
 	fmt.Printf("Hello, go!\n")
+
+	//Example
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
