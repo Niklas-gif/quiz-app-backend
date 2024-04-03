36f5fc0f8912de2365a034730083dc87dad88203
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-04-03 17:02:21 +0200

    Test insert to mongodb

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"quiz-app/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	client     *mongo.Client
+	db         *mongo.Database
+	collection *mongo.Collection
 )
 
 func init() {
@@ -39,5 +46,45 @@ func initMongoDB() {
 		panic(err)
 	}
 
+	db = client.Database("your_database_name")
+	collection = db.Collection("your_collection_name")
+	insertTest()
 	fmt.Println(client)
+}
+
+/*func insertTest() {
+	var _, err = collection.InsertOne(context.Background(), bson.M{"message": "This is a sample message from MongoDB"})
+	if err != nil {
+		log.Fatal("Failed to insert sample data into MongoDB:", err)
+	}
+}*/
+
+func insertTest() {
+	// Sample quiz data
+	sampleQuiz := models.Quiz{
+		QuizDescription: "Sample Quiz",
+		Questions: []models.Question{{
+			Description:      "Was ist die Hauptstadt von Deutschland?",
+			IsMultipleChoice: true,
+			Answers: []models.Answer{
+				{Description: "Paris", IsCorrect: false},
+				{Description: "London", IsCorrect: false},
+				{Description: "Berlin", IsCorrect: true},
+			},
+		}, {
+			Description:      "Was ist 2 + 2?",
+			IsMultipleChoice: false,
+			Answers: []models.Answer{
+				{Description: "4", IsCorrect: true},
+				{Description: "42", IsCorrect: false},
+				{Description: "Banana", IsCorrect: true},
+			},
+		},
+		},
+	}
+	// Inserting sample quiz data into MongoDB
+	_, err := collection.InsertOne(context.Background(), sampleQuiz)
+	if err != nil {
+		log.Fatal("Failed to insert sample data into MongoDB:", err)
+	}
 }
--- models/quiz.go
+++ models/quiz.go
@@ -1,16 +1,16 @@
 package models
 
-//TODO
+//TODO ID
 
 type Quiz struct {
-	QuizDescription string `json: "description" bson: "description"`
-	Questions       []Question
+	QuizDescription string     `json: "description" bson: "description"`
+	Questions       []Question `json:"questions" bson:"questions"`
 }
 
 type Question struct {
-	Description      string `json: "description" bson: "description"`
-	IsMultipleChoice bool   `json: "isMultipleChoice bson: is_multiple_choice"`
-	Answers          []Answer
+	Description      string   `json: "description" bson: "description"`
+	IsMultipleChoice bool     `json: "isMultipleChoice bson: is_multiple_choice"`
+	Answers          []Answer `json:"answers" bson:"answers"`
 }
 
 type Answer struct {
