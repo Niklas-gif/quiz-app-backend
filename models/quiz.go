c85c94b9be6567ec17aa1c2e1ddb41260b453555
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-04-03 17:28:52 +0200

    fixed syntax

--- models/quiz.go
+++ models/quiz.go
@@ -3,17 +3,17 @@ package models
 //TODO ID
 
 type Quiz struct {
-	QuizDescription string     `json: "description" bson: "description"`
+	QuizDescription string     `json:"description" bson:"description"`
 	Questions       []Question `json:"questions" bson:"questions"`
 }
 
 type Question struct {
-	Description      string   `json: "description" bson: "description"`
-	IsMultipleChoice bool     `json: "isMultipleChoice bson: is_multiple_choice"`
+	Description      string   `json:"description" bson:"description"`
+	IsMultipleChoice bool     `json:"isMultipleChoice" bson:"is_multiple_choice"`
 	Answers          []Answer `json:"answers" bson:"answers"`
 }
 
 type Answer struct {
-	Description string `json: "description" bson: "description"`
-	IsCorrect   bool   `json: "isCorrect" bson: "is_correct"`
+	Description string `json:"description" bson:"description"`
+	IsCorrect   bool   `json:"isCorrect" bson:"is_correct"`
 }
