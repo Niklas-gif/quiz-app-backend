b3d1f85c8725d0a1b2aeb670f14f2e07306d7809
Niklas D <niklas.demel1@gmail.com>
2024-05-22 15:52:26 +0200

    Update quiz.go

--- quizmodel/quiz.go
+++ quizmodel/quiz.go
@@ -8,11 +8,11 @@ type Quiz struct {
 
 type Question struct {
 	Description      string   `json:"description" bson:"description"`
-	IsMultipleChoice bool     `json:"isMultipleChoice" bson:"is_multiple_choice"`
+	IsMultipleChoice bool     `json:"is_multiple_choice" bson:"is_multiple_choice"`
 	Answers          []Answer `json:"answers" bson:"answers"`
 }
 
 type Answer struct {
 	Description string `json:"description" bson:"description"`
-	IsCorrect   bool   `json:"isCorrect" bson:"is_correct"`
+	IsCorrect   bool   `json:"is_correct" bson:"is_correct"`
 }
