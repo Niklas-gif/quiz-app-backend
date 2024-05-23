58e712db202ffe88d829c2cb4ca44248e313006c
Niklas D <niklas.demel1@gmail.com>
2024-05-23 10:00:50 +0200

    basic authentication token

--- usermodel/user.go
+++ usermodel/user.go
@@ -1,7 +1,7 @@
 package usermodel
 
 type User struct {
-	ID       int    `json: "id" bson: "id"`
+	ID       uint   `json: "id" bson: "id"`
 	Email    string `json:"email" bson:"email"`
 	Password string `json:"password" bson:"password"`
 }
