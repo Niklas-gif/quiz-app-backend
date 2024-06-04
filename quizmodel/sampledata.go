0bdf4cc02ab430fb49bab68e8fea78b584b480f4
Niklas D <niklas.demel1@gmail.com>
2024-06-04 14:33:49 +0200

    seeder

--- quizmodel/sampledata.go
+++ quizmodel/sampledata.go
@@ -23,4 +23,58 @@ var (
 		},
 		},
 	}
+	ExampleQuizzes = []Quiz{
+		{
+			QuizName:        "*SEEDER QUIZ* Science Quiz *SEEDER QUIZ*",
+			QuizDescription: "Test your science knowledge",
+			Questions: []Question{
+				{
+					Description:      "What is the chemical symbol for water?",
+					IsMultipleChoice: true,
+					Answers: []Answer{
+						{Description: "H2O", IsCorrect: true},
+						{Description: "O2", IsCorrect: false},
+						{Description: "CO2", IsCorrect: false},
+						{Description: "HO", IsCorrect: false},
+					},
+				},
+				{
+					Description:      "What planet is known as the Red Planet?",
+					IsMultipleChoice: true,
+					Answers: []Answer{
+						{Description: "Mars", IsCorrect: true},
+						{Description: "Venus", IsCorrect: false},
+						{Description: "Jupiter", IsCorrect: false},
+						{Description: "Saturn", IsCorrect: false},
+					},
+				},
+			},
+		},
+		{
+			QuizName:        "*SEEDER QUIZ* Math Quiz *SEEDER QUIZ*",
+			QuizDescription: "Test your math knowledge",
+			Questions: []Question{
+				{
+					Description:      "What is 2 + 2?",
+					IsMultipleChoice: true,
+					Answers: []Answer{
+						{Description: "4", IsCorrect: true},
+						{Description: "22", IsCorrect: false},
+						{Description: "3", IsCorrect: false},
+						{Description: "5", IsCorrect: false},
+					},
+				},
+				{
+					Description:      "What is the square root of 9?",
+					IsMultipleChoice: true,
+					Answers: []Answer{
+						{Description: "3", IsCorrect: true},
+						{Description: "81", IsCorrect: false},
+						{Description: "9", IsCorrect: false},
+						{Description: "27", IsCorrect: false},
+					},
+				},
+			},
+		},
+	}
 )
